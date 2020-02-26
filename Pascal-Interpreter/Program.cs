8d44b63e0b3fd99d584f30c74c21518ea5d614f2
ekutukcu <e.kutukcu@outlook.com>
2020-02-26 18:26:02 +0000

    Initial commit.

---
+++ Pascal-Interpreter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pascal_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
